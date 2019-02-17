8624dee08ae1d41bf5241a07b2e35b2bd67b81d0
José Miguel Martínez <josemiguel.martinez@isetic.com>
2019-02-17 20:54:35 +0100

    probar otras librerias

--- common/tones.py
+++ common/tones.py
@@ -1,14 +1,30 @@
-def interval_factor(half_steps):
-    return 2 ** (half_steps / 12)
+class Tones:
+    A4_freq = 440.0
 
 
-fifth_1 = 3 / 2
-fifth_2 = interval_factor(7.0)
-A4_freq = 440.0
+class Intervals:
+    @staticmethod
+    def interval_factor(half_steps):
+        return 2 ** (half_steps / 12)
+
+    fifth_1 = 3 / 2
+
+    @classmethod
+    def get_fifth_factor_2(cls):
+        fifth_2 = cls.interval_factor(7.0)
+        return fifth_2;
 
 
-def acorde(freq):
-    return [float(freq), float(interval_factor(7) * freq), float(interval_factor(4) * freq)]
+class Chord:
+    class Type:
+        major = 1, 4, 7
+        minor = 1, 3, 7
+        dim = 1, 3, 6
+        aug = 1, 4, 8
 
+    @classmethod
+    def chord_major_freqs(cls, freq, mults):
+        return [float(freq) * mults[0], float(Intervals.interval_factor(mults[1]) * freq),
+                float(Intervals.interval_factor(mults[2]) * freq)]
 
-C_major_chord = [261.626, 329.628, 391.996]
+    C_major_chord = [261.626, 329.628, 391.996]
--- use_synthesizer/synth_mod.py
+++ use_synthesizer/synth_mod.py
@@ -1,15 +1,16 @@
 from synthesizer import Player, Synthesizer, Waveform
 
-from common.tones import A4_freq, C_major_chord
+from common.tones import Chord, Intervals, Tones
+
 from common import tones
 
 player = Player()
 player.open_stream()
 synthesizer = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2=False)
 # Play A4
-player.play_wave(synthesizer.generate_constant_wave(A4_freq, 1.0))
+player.play_wave(synthesizer.generate_constant_wave(Tones.A4_freq, 1.0))
 
-player.play_wave(synthesizer.generate_chord(tones.acorde(A4_freq), 2.0))
+player.play_wave(synthesizer.generate_chord(tones.Chord.chord_major_freqs(Tones.A4_freq, Chord.Type.major), 2.0))
 
 # Play C major
-player.play_wave(synthesizer.generate_chord(C_major_chord, 3.0))
+player.play_wave(synthesizer.generate_chord(Chord.C_major_chord, 3.0))
