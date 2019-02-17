96d3cd6a3526d1a9e004544d48e84737cf44ed5a
José Miguel Martínez <josemiguel.martinez@isetic.com>
2019-02-17 20:14:32 +0100

    probar otras librerias

--- common/tones.py
+++ common/tones.py
@@ -4,3 +4,5 @@ def interval_factor(half_steps):
 
 fifth_1 = 3 / 2
 fifth_2 = interval_factor(7.0)
+A4_freq=440.0
+C_major_chord = [261.626,  329.628, 391.996]
--- use_synthesizer/synth_mod.py
+++ use_synthesizer/synth_mod.py
@@ -1,12 +1,12 @@
 from synthesizer import Player, Synthesizer, Waveform
 
+from common.tones import A4_freq, C_major_chord
 
 player = Player()
 player.open_stream()
 synthesizer = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2=False)
 # Play A4
-player.play_wave(synthesizer.generate_constant_wave(440.0, 3.0))
+player.play_wave(synthesizer.generate_constant_wave(A4_freq, 3.0))
 
 # Play C major
-chord = [261.626,  329.628, 391.996]
-player.play_wave(synthesizer.generate_chord(chord, 3.0))
+player.play_wave(synthesizer.generate_chord(C_major_chord, 3.0))
