308c43e59c2fcacaa8cf0d3ec9f1a225a8a1f217
José Miguel Martínez <josemiguel.martinez@isetic.com>
2019-02-17 20:19:45 +0100

    generar acordes

--- common/tones.py
+++ common/tones.py
@@ -4,5 +4,11 @@ def interval_factor(half_steps):
 
 fifth_1 = 3 / 2
 fifth_2 = interval_factor(7.0)
-A4_freq=440.0
-C_major_chord = [261.626,  329.628, 391.996]
+A4_freq = 440.0
+
+
+def acorde(freq):
+    return [float(freq), float(interval_factor(7) * freq), float(interval_factor(4) * freq)]
+
+
+C_major_chord = [261.626, 329.628, 391.996]
--- use_synthesizer/synth_mod.py
+++ use_synthesizer/synth_mod.py
@@ -1,12 +1,15 @@
 from synthesizer import Player, Synthesizer, Waveform
 
 from common.tones import A4_freq, C_major_chord
+from common import tones
 
 player = Player()
 player.open_stream()
 synthesizer = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2=False)
 # Play A4
-player.play_wave(synthesizer.generate_constant_wave(A4_freq, 3.0))
+player.play_wave(synthesizer.generate_constant_wave(A4_freq, 1.0))
+
+player.play_wave(synthesizer.generate_chord(tones.acorde(A4_freq), 2.0))
 
 # Play C major
 player.play_wave(synthesizer.generate_chord(C_major_chord, 3.0))
