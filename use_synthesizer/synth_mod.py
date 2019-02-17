c5ba6abb624a9b9caf8e1401c8872053a994c4d4
José Miguel Martínez <josemiguel.martinez@isetic.com>
2019-02-17 23:49:29 +0100

    probar otras librerias 2

--- use_synthesizer/synth_mod.py
+++ use_synthesizer/synth_mod.py
@@ -11,6 +11,9 @@ synthesizer = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2
 player.play_wave(synthesizer.generate_constant_wave(Tones.A4_freq, 1.0))
 
 player.play_wave(synthesizer.generate_chord(tones.Chord.chord_major_freqs(Tones.A4_freq, Chord.Type.major), 2.0))
+player.play_wave(synthesizer.generate_chord(tones.Chord.chord_major_freqs(Tones.A4_freq, Chord.Type.minor), 2.0))
+player.play_wave(synthesizer.generate_chord(tones.Chord.chord_major_freqs(Tones.A4_freq, Chord.Type.dim), 2.0))
+player.play_wave(synthesizer.generate_chord(tones.Chord.chord_major_freqs(Tones.A4_freq, Chord.Type.aug), 2.0))
 
 # Play C major
 player.play_wave(synthesizer.generate_chord(Chord.C_major_chord, 3.0))
