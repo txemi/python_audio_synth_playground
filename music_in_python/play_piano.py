7ce29b922fc68c326da344a02d1e2fadc32f244c
txemi <txemitron@gmail.com>
2021-06-21 04:08:05 +0200

    refactor

--- music_in_python/play_piano.py
+++ music_in_python/play_piano.py
@@ -3,9 +3,9 @@ from common.play.from_pytheory import playKatiNote
 
 note_freqs = utils.get_piano_notes()
 
-for a in note_freqs:
-    if not a:
+for note in note_freqs:
+    if not note:
         continue
-    b=note_freqs[a]
-    playKatiNote(a, b)
+    freq = note_freqs[note]
+    playKatiNote(note, freq)
     pass
