7eea1dbe649b9b27bee09fc9587095c2551772a2
txemi <txemitron@gmail.com>
2021-06-21 22:09:23 +0200

    refactor

--- learn_synthesizer/synth_mod.py
+++ learn_synthesizer/synth_mod.py
@@ -10,17 +10,17 @@ player = Player()
 player.open_stream()
 synthesizer = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2=False)
 
-uu = synthesizer.generate_constant_wave(TxTones.A4_freq, 1.0)
-player.play_wave(uu)
+wave_A4 = synthesizer.generate_constant_wave(TxTones.A4_freq, 1.0)
+player.play_wave(wave_A4)
 
 player.play_wave(synthesizer.generate_chord(TxTones.A4, 2.0))
 
-iu = synthesizer.generate_chord(TxChord.c3_major_chord_names, 3.0)
-player.play_wave(iu)
+wave_C3_major_chord = synthesizer.generate_chord(TxChord.c3_major_chord_names, 3.0)
+player.play_wave(wave_C3_major_chord)
 
 player.play_wave(synthesizer.generate_chord(TxChord.C4_major_chord_freqs, 3.0))
 
-for chord1 in TxChord.Type.all:
-    rrr =list( txintervals.TxChord.freqs_mult(TxTones.A4_freq, chord1))
-    aaa = synthesizer.generate_chord(rrr, 2.0)
-    player.play_wave(aaa)
+for current_chord in TxChord.Type.all:
+    freqs =list(txintervals.TxChord.freqs_mult(TxTones.A4_freq, current_chord))
+    chord_wave = synthesizer.generate_chord(freqs, 2.0)
+    player.play_wave(chord_wave)
