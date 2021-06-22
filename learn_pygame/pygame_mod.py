9b21cae116f498bdf807b6f67e7f98d1111cac3a
txemi <txemitron@gmail.com>
2021-06-22 17:36:38 +0200

    refactor

--- learn_pygame/pygame_mod.py
+++ learn_pygame/pygame_mod.py
@@ -7,8 +7,8 @@ from common.txtone import TxTones
 pygame.mixer.init(frequency=sample_rate_22050, size=-16, channels=1, buffer=4096)
 #pygame.mixer.Sound.set_volume(1.0)
 
-# Play A (440Hz) for 1 second as a sine synt_wave:
+# Play A (440Hz) for 1 second as a sine wave:
 play_for(sine_wave(TxTones.A4_freq, 4096), 1000)
 
-# Play A-440 for 1 second as a square synt_wave:
+# Play A-440 for 1 second as a square wave:
 play_for(square_wave(TxTones.A4_freq, 4096), 1000)
