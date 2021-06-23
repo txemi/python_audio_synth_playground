7efb69ac19ee4bbfbee7b83a73f0b33f3fc093fe
txemi <txemitron@gmail.com>
2021-06-23 11:18:46 +0200

    refactor

--- learn_pytheory/t20_scale.py
+++ learn_pytheory/t20_scale.py
@@ -1,13 +1,6 @@
 # https://github.com/kennethreitz/pytheory
 from pytheory import TonedScale, Tone
-c4_tone = Tone.from_string('C4')
-c4_pitch_s=c4_tone.pitch(symbolic=True)
-met=dir(c4_pitch_s)
-c4_pitch=c4_pitch_s.evalf()
 
-c5_tone = Tone.from_string('C5')
-c5_pitch_s=c5_tone.pitch(symbolic=True)
-c5_pitch_n=c5_pitch_s.evalf()
 
 # From scale
 ts=TonedScale(tonic='C4')
