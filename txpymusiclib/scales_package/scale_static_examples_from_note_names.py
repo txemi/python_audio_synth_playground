63ba3ee24de519976aebba9e36bf280cf2b186cf
txemi <txemitron@gmail.com>
2021-06-24 11:28:21 +0200

    refactor

--- txpymusiclib/scales_package/scale_static_examples_from_note_names.py
+++ txpymusiclib/scales_package/scale_static_examples_from_note_names.py
@@ -1,1 +1,6 @@
+import txpymusiclib
+
 c_major_scale = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5']
+# TODO: use object when possible
+c_major_scale_notes = list(txpymusiclib.scales_package.scale_mingus.mingus_names_to_notes(
+    txpymusiclib.scales_package.scale_static_examples_from_note_names.c_major_scale))
