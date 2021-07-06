307f0b57ed07f08c775bb3ee31d5e315470e0925
txemi <txemitron@gmail.com>
2021-07-06 23:03:30 +0200

    listen some progressions

--- learn_libs/learn_mingus/t30_2_2_play_chords.py
+++ learn_libs/learn_mingus/t30_2_2_play_chords.py
@@ -1,20 +1,24 @@
+import time
+
 import txpymusiclib.play.play_mingus_in_synthesizer
 from txpymusiclib.chords_package import chord_progression_examples
 
 # from mingus.midi import fluidsynth
 
-
-for a in chord_progression_examples.chord_progression_examples:
-    print(a)
-    txpymusiclib.play.play_mingus_in_synthesizer.play_progressions_roman(a)
-    print('\n')
-
-txpymusiclib.play.play_mingus_in_synthesizer.play_progressions_roman(
-    chord_progression_examples.chord_progression_example_1)
-
-txpymusiclib.play.play_mingus_in_synthesizer.play_chords_loop_chord_notation(chord_progression_examples.LordOfRings, 3)
-
 txpymusiclib.play.play_mingus_in_synthesizer.play_chords_loop_chord_notation(
     chord_progression_examples.NiceChordSeqExample, 3)
+time.sleep(1)
+
+txpymusiclib.play.play_mingus_in_synthesizer.play_chords_loop_chord_notation(chord_progression_examples.LordOfRings, 3)
+time.sleep(1)
+
+chord_progression_examples.chord_progression_example_1.progression_test()
+time.sleep(1)
+
+for current_progression in chord_progression_examples.chord_progression_examples:
+    print(current_progression)
+    current_progression.progression_test()
+    print('\n')
+    time.sleep(1)
 
 print(1)
